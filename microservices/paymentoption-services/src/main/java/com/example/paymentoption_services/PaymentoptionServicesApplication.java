c24402a02fdae1cb80ce53fc6ef92b9fe459228b
aessien18 <angelaessein@gmail.com>
2025-07-22 10:53:27 +0000

    payement option

---
+++ microservices/paymentoption-services/src/main/java/com/example/paymentoption_services/PaymentoptionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentoption_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentoptionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentoptionServicesApplication.class, args);
+	}
+
+}
---
+++ microservices/paymentoption-services/src/test/java/com/example/paymentoption_services/PaymentoptionServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentoption_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentoptionServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
